689f97e2f1fb9a2659ccd9ca405f5a361aa7f972
ryskiwt <iw.06.ry.10@gmail.com>
2019-02-05 11:33:12 +0900

    debug

--- executor/http_runner.go
+++ executor/http_runner.go
@@ -1,6 +1,7 @@
 package executor
 
 import (
+	"bufio"
 	"bytes"
 	"context"
 	"fmt"
@@ -9,7 +10,6 @@ import (
 	"log"
 	"net"
 	"net/http"
-	"net/http/httputil"
 	"net/url"
 	"os"
 	"os/exec"
@@ -174,8 +174,21 @@ func (f *HTTPFunctionRunner) Run(req FunctionRequest, contentLength int64, r *ht
 	if res.Body != nil {
 		defer res.Body.Close()
 
-		if _, bodyErr := io.Copy(httputil.NewChunkedWriter(w), res.Body); bodyErr != nil {
-			log.Println("read body err", bodyErr)
+		if res.ContentLength == -1 {
+			scanner := bufio.NewScanner(res.Body)
+			for scanner.Scan() {
+				if _, bodyErr := fmt.Fprintln(w, scanner.Text()); bodyErr != nil {
+					log.Println("read body err", bodyErr)
+				}
+				w.(http.Flusher).Flush()
+			}
+			if scanErr := scanner.Err(); scanErr != nil {
+				log.Println("read body err", scanErr)
+			}
+		} else {
+			if _, bodyErr := io.Copy(w, res.Body); bodyErr != nil {
+				log.Println("read body err", bodyErr)
+			}
 		}
 	}
 
